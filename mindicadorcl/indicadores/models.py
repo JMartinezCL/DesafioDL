f05a38b573e98e6e5358c841295511b773fcb148
JMartinezCL <martinez.alvarez.jose77@gmail.com>
2019-10-10 03:01:18 -0300

    nuevo html para guardar los datos historicos del indicador elejido por el usuario

--- mindicadorcl/indicadores/models.py
+++ mindicadorcl/indicadores/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class uf(models.Model):
+    id_uf = models.AutoField(primary_key=True)
+    valor = models.FloatField(null=False)
+    fecha = models.DateField(max_length=24, null=False)
+
+
+class ipc(models.Model):
+    id_ipc = models.AutoField(primary_key=True)
+    valor = models.FloatField(null=False)
+    fecha = models.CharField(max_length=24, null=False)
--- mindicadorcl/indicadores/views.py
+++ mindicadorcl/indicadores/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
+
+
+def consumoApi(request):
+
+    response = requests.get('https://www.mindicador.cl/api/uf')
+    # print(response.text)
+    response_json = json.loads(response.text)
+    uf_valor = response_json['serie'][0]
+    print(uf_valor)
+
+    return render(request, 'indicadores/indicadores_api.html', {})
